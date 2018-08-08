91bf6e2d16744e3bbce6f21346d1c533e3ff8264
Mouse <937279156@qq.com>
2018-08-09 01:20:22 +0800

    Added ores

--- src/main/java/cn/mccraft/pangu/base/init/PBasicBlocks.java
+++ src/main/java/cn/mccraft/pangu/base/init/PBasicBlocks.java
@@ -1,8 +1,33 @@
 package cn.mccraft.pangu.base.init;
 
+import cn.mccraft.pangu.core.block.PGBlock;
+import cn.mccraft.pangu.core.loader.annotation.RegBlock;
 import cn.mccraft.pangu.core.loader.creativetabs.SharedCreativeTab;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 
 @SharedCreativeTab("panguBase")
 public interface PBasicBlocks {
 
+    @RegBlock(value = {"copper", "ore"}, oreDict = {"oreCopper"})
+    Block COPPER_ORE = new PGBlock(Material.ROCK).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(5.0F);
+
+    @RegBlock(value = {"tin", "ore"}, oreDict = {"oreTin"})
+    Block TIN_ORE = new PGBlock(Material.ROCK).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(5.0F);
+
+    @RegBlock(value = {"silver", "ore"}, oreDict = {"oreSilver"})
+    Block SILVER_ORE = new PGBlock(Material.ROCK).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(5.0F);
+
+    @RegBlock(value = {"copper", "block"}, oreDict = {"blockCopper"})
+    Block COPPER_BLOCK = new PGBlock(Material.IRON).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(10.0F);
+
+    @RegBlock(value = {"tin", "block"}, oreDict = {"blockTin"})
+    Block TIN_BLOCK = new PGBlock(Material.IRON).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(10.0F);
+
+    @RegBlock(value = {"bronze", "block"}, oreDict = {"blockBronze"})
+    Block BRONZE_BLOCK = new PGBlock(Material.IRON).setHarvestLevelR("pickaxe", 1).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(10.0F);
+
+    @RegBlock(value = {"silver", "block"}, oreDict = {"blockSilver"})
+    Block SILVER_BLOCK = new PGBlock(Material.IRON).setHarvestLevelR("pickaxe", 2).setSoundType(SoundType.STONE).setHardness(3.0F).setResistance(10.0F);
 }
--- src/main/java/cn/mccraft/pangu/base/init/PBasicItems.java
+++ src/main/java/cn/mccraft/pangu/base/init/PBasicItems.java
@@ -1,8 +1,23 @@
 package cn.mccraft.pangu.base.init;
 
+import cn.mccraft.pangu.core.item.PGItem;
+import cn.mccraft.pangu.core.loader.annotation.RegItem;
 import cn.mccraft.pangu.core.loader.creativetabs.SharedCreativeTab;
+import net.minecraft.item.Item;
 
 @SharedCreativeTab("panguBase")
 public interface PBasicItems {
+
+    @RegItem(value = {"copper", "ingot"}, oreDict = {"ingotCopper"})
+    Item COPPER_INGOT = new PGItem();
+
+    @RegItem(value = {"tin", "ingot"}, oreDict = {"ingotTin"})
+    Item TIN_INGOT = new PGItem();
+
+    @RegItem(value = {"bronze", "ingot"}, oreDict = {"ingotBronze"})
+    Item BRONZE_INGOT = new PGItem();
+
+    @RegItem(value = {"silver", "ingot"}, oreDict = {"ingotSilver"})
+    Item SILVER_INGOT = new PGItem();
 
 }
